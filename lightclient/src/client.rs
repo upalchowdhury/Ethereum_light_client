2a5ea33a83931aa74f19dc1d326afa4315cd9480
upalchowdhury <upal.chowdhury@outlook.com>
2023-01-06 08:56:08 -0500

    node for local evm execution

---
+++ lightclient/src/client.rs
@@ -0,0 +1,438 @@
+use std::path::PathBuf;
+use std::sync::Arc;
+
+use config::networks::Network;
+use ethers::prelude::{Address, U256};
+use ethers::types::{Filter, Log, Transaction, TransactionReceipt, H256};
+use eyre::{eyre, Result};
+
+use common::types::BlockTag;
+use config::{CheckpointFallback, Config};
+use consensus::{types::Header, ConsensusClient};
+use execution::types::{CallOpts, ExecutionBlock};
+use log::{info, warn};
+use tokio::spawn;
+use tokio::sync::RwLock;
+use tokio::time::sleep;
+
+use crate::database::{Database, FileDB};
+use crate::node::Node;
+use crate::rpc::Rpc;
+
+#[derive(Default)]
+pub struct ClientBuilder {
+    network: Option<Network>,
+    consensus_rpc: Option<String>,
+    execution_rpc: Option<String>,
+    checkpoint: Option<Vec<u8>>,
+    rpc_port: Option<u16>,
+    data_dir: Option<PathBuf>,
+    config: Option<Config>,
+    fallback: Option<String>,
+    load_external_fallback: bool,
+}
+
+impl ClientBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn network(mut self, network: Network) -> Self {
+        self.network = Some(network);
+        self
+    }
+
+    pub fn consensus_rpc(mut self, consensus_rpc: &str) -> Self {
+        self.consensus_rpc = Some(consensus_rpc.to_string());
+        self
+    }
+
+    pub fn execution_rpc(mut self, execution_rpc: &str) -> Self {
+        self.execution_rpc = Some(execution_rpc.to_string());
+        self
+    }
+
+    pub fn checkpoint(mut self, checkpoint: &str) -> Self {
+        let checkpoint = hex::decode(checkpoint.strip_prefix("0x").unwrap_or(checkpoint))
+            .expect("cannot parse checkpoint");
+        self.checkpoint = Some(checkpoint);
+        self
+    }
+
+    pub fn rpc_port(mut self, port: u16) -> Self {
+        self.rpc_port = Some(port);
+        self
+    }
+
+    pub fn data_dir(mut self, data_dir: PathBuf) -> Self {
+        self.data_dir = Some(data_dir);
+        self
+    }
+
+    pub fn config(mut self, config: Config) -> Self {
+        self.config = Some(config);
+        self
+    }
+
+    pub fn fallback(mut self, fallback: &str) -> Self {
+        self.fallback = Some(fallback.to_string());
+        self
+    }
+
+    pub fn load_external_fallback(mut self) -> Self {
+        self.load_external_fallback = true;
+        self
+    }
+
+    pub fn build(self) -> Result<Client<FileDB>> {
+        let base_config = if let Some(network) = self.network {
+            network.to_base_config()
+        } else {
+            let config = self
+                .config
+                .as_ref()
+                .ok_or(eyre!("missing network config"))?;
+            config.to_base_config()
+        };
+
+        let consensus_rpc = self.consensus_rpc.unwrap_or_else(|| {
+            self.config
+                .as_ref()
+                .expect("missing consensus rpc")
+                .consensus_rpc
+                .clone()
+        });
+
+        let execution_rpc = self.execution_rpc.unwrap_or_else(|| {
+            self.config
+                .as_ref()
+                .expect("missing execution rpc")
+                .execution_rpc
+                .clone()
+        });
+
+        let checkpoint = if let Some(checkpoint) = self.checkpoint {
+            checkpoint
+        } else if let Some(config) = &self.config {
+            config.checkpoint.clone()
+        } else {
+            base_config.checkpoint
+        };
+
+        let rpc_port = if self.rpc_port.is_some() {
+            self.rpc_port
+        } else if let Some(config) = &self.config {
+            config.rpc_port
+        } else {
+            None
+        };
+
+        let data_dir = if self.data_dir.is_some() {
+            self.data_dir
+        } else if let Some(config) = &self.config {
+            config.data_dir.clone()
+        } else {
+            None
+        };
+
+        let fallback = if self.fallback.is_some() {
+            self.fallback
+        } else if let Some(config) = &self.config {
+            config.fallback.clone()
+        } else {
+            None
+        };
+
+        let load_external_fallback = if let Some(config) = &self.config {
+            self.load_external_fallback || config.load_external_fallback
+        } else {
+            self.load_external_fallback
+        };
+
+        let config = Config {
+            consensus_rpc,
+            execution_rpc,
+            checkpoint,
+            rpc_port,
+            data_dir,
+            chain: base_config.chain,
+            forks: base_config.forks,
+            max_checkpoint_age: base_config.max_checkpoint_age,
+            fallback,
+            load_external_fallback,
+        };
+
+        Client::new(config)
+    }
+}
+
+pub struct Client<DB: Database> { 
+    // DB can be of anytype as long as implements Database trait. Its called trait bound
+    // https://doc.rust-lang.org/book/ch10-02-traits.html#trait-bound-syntax
+    
+    /*
+    The "impl" Trait syntax is convenient and makes for more concise code in simple cases, while the fuller trait bound syntax can express more complexity in other cases. For example, we can have two parameters that implement Summary. Doing so with the impl Trait syntax looks like this:
+
+pub fn notify(item1: &impl Summary, item2: &impl Summary) {
+Using impl Trait is appropriate if we want this function to allow item1 and item2 to have different types (as long as both types implement Summary). If we want to force both parameters to have the same type, however, we must use a trait bound, like this:
+
+pub fn notify<T: Summary>(item1: &T, item2: &T) {
+
+    https://doc.rust-lang.org/book/ch10-02-traits.html#clearer-trait-bounds-with-where-clausesafj;sldk qoijfdfjkjf
+    
+    */
+    
+    
+    node: Arc<RwLock<Node>>,
+    rpc: Option<Rpc>,
+    db: Option<DB>,
+    fallback: Option<String>,
+    load_external_fallback: bool,
+}
+
+impl Client<FileDB> {
+    fn new(config: Config) -> Result<Self> {
+        let config = Arc::new(config);
+        let node = Node::new(config.clone())?;
+        let node = Arc::new(RwLock::new(node));
+
+        let rpc = config.rpc_port.map(|port| Rpc::new(node.clone(), port));
+
+        let data_dir = config.data_dir.clone();
+        let db = data_dir.map(FileDB::new);
+
+        Ok(Client {
+            node,
+            rpc,
+            db,
+            fallback: config.fallback.clone(),
+            load_external_fallback: config.load_external_fallback,
+        })
+    }
+}
+
+impl<DB: Database> Client<DB> { 
+    // https://doc.rust-lang.org/book/ch10-01-syntax.html#in-method-definitions
+    pub async fn start(&mut self) -> Result<()> {
+        if let Some(rpc) = &mut self.rpc {
+            rpc.start().await?;
+        }
+
+        if self.node.write().await.sync().await.is_err() {
+            warn!(
+                "failed to sync consensus node with checkpoint: 0x{}",
+                hex::encode(&self.node.read().await.config.checkpoint),
+            );
+            let fallback = self.boot_from_fallback().await;
+            if fallback.is_err() && self.load_external_fallback {
+                self.boot_from_external_fallbacks().await?
+            } else if fallback.is_err() {
+                return Err(eyre::eyre!("Checkpoint is too old. Please update your checkpoint. Alternatively, set an explicit checkpoint fallback service url with the `-f` flag or use the configured external fallback services with `-l` (NOT RECOMMENED). See https://github.com/a16z/helios#additional-options for more information."));
+            }
+        }
+
+        let node = self.node.clone();
+        spawn(async move {
+            loop {
+                let res = node.write().await.advance().await;
+                if let Err(err) = res {
+                    warn!("consensus error: {}", err);
+                }
+
+                let next_update = node.read().await.duration_until_next_update();
+                sleep(next_update).await;
+            }
+        });
+
+        Ok(())
+    }
+
+    async fn boot_from_fallback(&self) -> eyre::Result<()> {
+        if let Some(fallback) = &self.fallback {
+            info!(
+                "attempting to load checkpoint from fallback \"{}\"",
+                fallback
+            );
+
+            let checkpoint = CheckpointFallback::fetch_checkpoint_from_api(fallback)
+                .await
+                .map_err(|_| {
+                    eyre::eyre!("Failed to fetch checkpoint from fallback \"{}\"", fallback)
+                })?;
+
+            info!(
+                "external fallbacks responded with checkpoint 0x{:?}",
+                checkpoint
+            );
+
+            // Try to sync again with the new checkpoint by reconstructing the consensus client
+            // mWe fail fast here since the node is unrecoverable at this point
+            let config = self.node.read().await.config.clone();
+            let consensus =
+                ConsensusClient::new(&config.consensus_rpc, checkpoint.as_bytes(), config.clone())?;
+            self.node.write().await.consensus = consensus;
+            self.node.write().await.sync().await?;
+
+            Ok(())
+        } else {
+            Err(eyre::eyre!("no explicit fallback specified"))
+        }
+    }
+
+    async fn boot_from_external_fallbacks(&self) -> eyre::Result<()> {
+        info!("attempting to fetch checkpoint from external fallbacks...");
+        // Build the list of external checkpoint fallback services
+        let list = CheckpointFallback::new()
+            .build()
+            .await
+            .map_err(|_| eyre::eyre!("Failed to construct external checkpoint sync fallbacks"))?;
+
+        let checkpoint = if self.node.read().await.config.chain.chain_id == 5 {
+            list.fetch_latest_checkpoint(&Network::GOERLI)
+                .await
+                .map_err(|_| {
+                    eyre::eyre!("Failed to fetch latest goerli checkpoint from external fallbacks")
+                })?
+        } else {
+            list.fetch_latest_checkpoint(&Network::MAINNET)
+                .await
+                .map_err(|_| {
+                    eyre::eyre!("Failed to fetch latest mainnet checkpoint from external fallbacks")
+                })?
+        };
+
+        info!(
+            "external fallbacks responded with checkpoint {:?}",
+            checkpoint
+        );
+
+        // Try to sync again with the new checkpoint by reconstructing the consensus client
+        // We fail fast here since the node is unrecoverable at this point
+        let config = self.node.read().await.config.clone();
+        let consensus =
+            ConsensusClient::new(&config.consensus_rpc, checkpoint.as_bytes(), config.clone())?;
+        self.node.write().await.consensus = consensus;
+        self.node.write().await.sync().await?;
+        Ok(())
+    }
+
+    pub async fn shutdown(&self) {
+        let node = self.node.read().await;
+        let checkpoint = if let Some(checkpoint) = node.get_last_checkpoint() {
+            checkpoint
+        } else {
+            return;
+        };
+
+        info!("saving last checkpoint hash");
+        let res = self.db.as_ref().map(|db| db.save_checkpoint(checkpoint));
+        if res.is_some() && res.unwrap().is_err() {
+            warn!("checkpoint save failed");
+        }
+    }
+
+    pub async fn call(&self, opts: &CallOpts, block: BlockTag) -> Result<Vec<u8>> {
+        self.node
+            .read()
+            .await
+            .call(opts, block)
+            .await
+            .map_err(|err| err.into())
+    }
+
+    pub async fn estimate_gas(&self, opts: &CallOpts) -> Result<u64> {
+        self.node
+            .read()
+            .await
+            .estimate_gas(opts)
+            .await
+            .map_err(|err| err.into())
+    }
+
+    pub async fn get_balance(&self, address: &Address, block: BlockTag) -> Result<U256> {
+        self.node.read().await.get_balance(address, block).await
+    }
+
+    pub async fn get_nonce(&self, address: &Address, block: BlockTag) -> Result<u64> {
+        self.node.read().await.get_nonce(address, block).await
+    }
+
+    pub async fn get_code(&self, address: &Address, block: BlockTag) -> Result<Vec<u8>> {
+        self.node.read().await.get_code(address, block).await
+    }
+
+    pub async fn get_storage_at(&self, address: &Address, slot: H256) -> Result<U256> {
+        self.node.read().await.get_storage_at(address, slot).await
+    }
+
+    pub async fn send_raw_transaction(&self, bytes: &[u8]) -> Result<H256> {
+        self.node.read().await.send_raw_transaction(bytes).await
+    }
+
+    pub async fn get_transaction_receipt(
+        &self,
+        tx_hash: &H256,
+    ) -> Result<Option<TransactionReceipt>> {
+        self.node
+            .read()
+            .await
+            .get_transaction_receipt(tx_hash)
+            .await
+    }
+
+    pub async fn get_transaction_by_hash(&self, tx_hash: &H256) -> Result<Option<Transaction>> {
+        self.node
+            .read()
+            .await
+            .get_transaction_by_hash(tx_hash)
+            .await
+    }
+
+    pub async fn get_logs(&self, filter: &Filter) -> Result<Vec<Log>> {
+        self.node.read().await.get_logs(filter).await
+    }
+
+    pub async fn get_gas_price(&self) -> Result<U256> {
+        self.node.read().await.get_gas_price()
+    }
+
+    pub async fn get_priority_fee(&self) -> Result<U256> {
+        self.node.read().await.get_priority_fee()
+    }
+
+    pub async fn get_block_number(&self) -> Result<u64> {
+        self.node.read().await.get_block_number()
+    }
+
+    pub async fn get_block_by_number(
+        &self,
+        block: BlockTag,
+        full_tx: bool,
+    ) -> Result<Option<ExecutionBlock>> {
+        self.node
+            .read()
+            .await
+            .get_block_by_number(block, full_tx)
+            .await
+    }
+
+    pub async fn get_block_by_hash(
+        &self,
+        hash: &Vec<u8>,
+        full_tx: bool,
+    ) -> Result<Option<ExecutionBlock>> {
+        self.node
+            .read()
+            .await
+            .get_block_by_hash(hash, full_tx)
+            .await
+    }
+
+    pub async fn chain_id(&self) -> u64 {
+        self.node.read().await.chain_id()
+    }
+
+    pub async fn get_header(&self) -> Result<Header> {
+        self.node.read().await.get_header()
+    }
+}
---
+++ lightclient/src/node.rs
@@ -0,0 +1,313 @@
+use std::collections::BTreeMap;
+use std::sync::Arc;
+use std::time::Duration;
+
+use ethers::prelude::{Address, U256};
+use ethers::types::{Filter, Log, Transaction, TransactionReceipt, H256};
+use eyre::{eyre, Result};
+
+use common::errors::BlockNotFoundError;
+use common::types::BlockTag;
+use config::Config;
+use consensus::rpc::nimbus_rpc::NimbusRpc;
+use consensus::types::{ExecutionPayload, Header};
+use consensus::ConsensusClient;
+use execution::evm::Evm;
+use execution::rpc::http_rpc::HttpRpc;
+use execution::types::{CallOpts, ExecutionBlock};
+use execution::ExecutionClient;
+
+use crate::errors::NodeError;
+
+pub struct Node {
+    pub consensus: ConsensusClient<NimbusRpc>,
+    pub execution: Arc<ExecutionClient<HttpRpc>>,
+    pub config: Arc<Config>,
+    payloads: BTreeMap<u64, ExecutionPayload>,
+    finalized_payloads: BTreeMap<u64, ExecutionPayload>,
+    pub history_size: usize,
+}
+
+impl Node {
+    pub fn new(config: Arc<Config>) -> Result<Self, NodeError> {
+        let consensus_rpc = &config.consensus_rpc;
+        let checkpoint_hash = &config.checkpoint;
+        let execution_rpc = &config.execution_rpc;
+
+        let consensus = ConsensusClient::new(consensus_rpc, checkpoint_hash, config.clone())
+            .map_err(NodeError::ConsensusClientCreationError)?;
+        let execution = Arc::new(
+            ExecutionClient::new(execution_rpc).map_err(NodeError::ExecutionClientCreationError)?,
+        );
+
+        let payloads = BTreeMap::new();
+        let finalized_payloads = BTreeMap::new();
+
+        Ok(Node {
+            consensus,
+            execution,
+            config,
+            payloads,
+            finalized_payloads,
+            history_size: 64,
+        })
+    }
+
+    pub async fn sync(&mut self) -> Result<(), NodeError> {
+        self.consensus
+            .sync()
+            .await
+            .map_err(NodeError::ConsensusSyncError)?;
+        self.update_payloads().await
+    }
+
+    pub async fn advance(&mut self) -> Result<(), NodeError> {
+        self.consensus
+            .advance()
+            .await
+            .map_err(NodeError::ConsensusAdvanceError)?;
+        self.update_payloads().await
+    }
+
+    pub fn duration_until_next_update(&self) -> Duration {
+        self.consensus
+            .duration_until_next_update()
+            .to_std()
+            .unwrap()
+    }
+
+    async fn update_payloads(&mut self) -> Result<(), NodeError> {
+        let latest_header = self.consensus.get_header();
+        let latest_payload = self
+            .consensus
+            .get_execution_payload(&Some(latest_header.slot))
+            .await
+            .map_err(NodeError::ConsensusPayloadError)?;
+
+        let finalized_header = self.consensus.get_finalized_header();
+        let finalized_payload = self
+            .consensus
+            .get_execution_payload(&Some(finalized_header.slot))
+            .await
+            .map_err(NodeError::ConsensusPayloadError)?;
+
+        self.payloads
+            .insert(latest_payload.block_number, latest_payload);
+        self.payloads
+            .insert(finalized_payload.block_number, finalized_payload.clone());
+        self.finalized_payloads
+            .insert(finalized_payload.block_number, finalized_payload);
+
+        while self.payloads.len() > self.history_size {
+            self.payloads.pop_first();
+        }
+
+        // only save one finalized block per epoch
+        // finality updates only occur on epoch boundries
+        while self.finalized_payloads.len() > usize::max(self.history_size / 32, 1) {
+            self.finalized_payloads.pop_first();
+        }
+
+        Ok(())
+    }
+
+    pub async fn call(&self, opts: &CallOpts, block: BlockTag) -> Result<Vec<u8>, NodeError> {
+        self.check_blocktag_age(&block)?;
+
+        let payload = self.get_payload(block)?;
+        let mut evm = Evm::new(
+            self.execution.clone(),
+            payload,
+            &self.payloads,
+            self.chain_id(),
+        );
+        evm.call(opts).await.map_err(NodeError::ExecutionError)
+    }
+
+    pub async fn estimate_gas(&self, opts: &CallOpts) -> Result<u64, NodeError> {
+        self.check_head_age()?;
+
+        let payload = self.get_payload(BlockTag::Latest)?;
+        let mut evm = Evm::new(
+            self.execution.clone(),
+            payload,
+            &self.payloads,
+            self.chain_id(),
+        );
+        evm.estimate_gas(opts)
+            .await
+            .map_err(NodeError::ExecutionError)
+    }
+
+    pub async fn get_balance(&self, address: &Address, block: BlockTag) -> Result<U256> {
+        self.check_blocktag_age(&block)?;
+
+        let payload = self.get_payload(block)?;
+        let account = self.execution.get_account(address, None, payload).await?;
+        Ok(account.balance)
+    }
+
+    pub async fn get_nonce(&self, address: &Address, block: BlockTag) -> Result<u64> {
+        self.check_blocktag_age(&block)?;
+
+        let payload = self.get_payload(block)?;
+        let account = self.execution.get_account(address, None, payload).await?;
+        Ok(account.nonce)
+    }
+
+    pub async fn get_code(&self, address: &Address, block: BlockTag) -> Result<Vec<u8>> {
+        self.check_blocktag_age(&block)?;
+
+        let payload = self.get_payload(block)?;
+        let account = self.execution.get_account(address, None, payload).await?;
+        Ok(account.code)
+    }
+
+    pub async fn get_storage_at(&self, address: &Address, slot: H256) -> Result<U256> {
+        self.check_head_age()?;
+
+        let payload = self.get_payload(BlockTag::Latest)?;
+        let account = self
+            .execution
+            .get_account(address, Some(&[slot]), payload)
+            .await?;
+
+        let value = account.slots.get(&slot);
+        match value {
+            Some(value) => Ok(*value),
+            None => Err(eyre!("slot not found")),
+        }
+    }
+
+    pub async fn send_raw_transaction(&self, bytes: &[u8]) -> Result<H256> {
+        self.execution.send_raw_transaction(bytes).await
+    }
+
+    pub async fn get_transaction_receipt(
+        &self,
+        tx_hash: &H256,
+    ) -> Result<Option<TransactionReceipt>> {
+        self.execution
+            .get_transaction_receipt(tx_hash, &self.payloads)
+            .await
+    }
+
+    pub async fn get_transaction_by_hash(&self, tx_hash: &H256) -> Result<Option<Transaction>> {
+        self.execution
+            .get_transaction(tx_hash, &self.payloads)
+            .await
+    }
+
+    pub async fn get_logs(&self, filter: &Filter) -> Result<Vec<Log>> {
+        self.execution.get_logs(filter, &self.payloads).await
+    }
+
+    // assumes tip of 1 gwei to prevent having to prove out every tx in the block
+    pub fn get_gas_price(&self) -> Result<U256> {
+        self.check_head_age()?;
+
+        let payload = self.get_payload(BlockTag::Latest)?;
+        let base_fee = U256::from_little_endian(&payload.base_fee_per_gas.to_bytes_le());
+        let tip = U256::from(10_u64.pow(9));
+        Ok(base_fee + tip)
+    }
+
+    // assumes tip of 1 gwei to prevent having to prove out every tx in the block
+    pub fn get_priority_fee(&self) -> Result<U256> {
+        let tip = U256::from(10_u64.pow(9));
+        Ok(tip)
+    }
+
+    pub fn get_block_number(&self) -> Result<u64> {
+        self.check_head_age()?;
+
+        let payload = self.get_payload(BlockTag::Latest)?;
+        Ok(payload.block_number)
+    }
+
+    pub async fn get_block_by_number(
+        &self,
+        block: BlockTag,
+        full_tx: bool,
+    ) -> Result<Option<ExecutionBlock>> {
+        self.check_blocktag_age(&block)?;
+
+        match self.get_payload(block) {
+            Ok(payload) => self.execution.get_block(payload, full_tx).await.map(Some),
+            Err(_) => Ok(None),
+        }
+    }
+
+    pub async fn get_block_by_hash(
+        &self,
+        hash: &Vec<u8>,
+        full_tx: bool,
+    ) -> Result<Option<ExecutionBlock>> {
+        let payloads = self
+            .payloads
+            .iter()
+            .filter(|entry| &entry.1.block_hash.to_vec() == hash)
+            .collect::<Vec<(&u64, &ExecutionPayload)>>();
+
+        if let Some(payload_entry) = payloads.get(0) {
+            self.execution
+                .get_block(payload_entry.1, full_tx)
+                .await
+                .map(Some)
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn chain_id(&self) -> u64 {
+        self.config.chain.chain_id
+    }
+
+    pub fn get_header(&self) -> Result<Header> {
+        self.check_head_age()?;
+        Ok(self.consensus.get_header().clone())
+    }
+
+    pub fn get_last_checkpoint(&self) -> Option<Vec<u8>> {
+        self.consensus.last_checkpoint.clone()
+    }
+
+    fn get_payload(&self, block: BlockTag) -> Result<&ExecutionPayload, BlockNotFoundError> {
+        match block {
+            BlockTag::Latest => {
+                let payload = self.payloads.last_key_value();
+                Ok(payload.ok_or(BlockNotFoundError::new(BlockTag::Latest))?.1)
+            }
+            BlockTag::Finalized => {
+                let payload = self.finalized_payloads.last_key_value();
+                Ok(payload
+                    .ok_or(BlockNotFoundError::new(BlockTag::Finalized))?
+                    .1)
+            }
+            BlockTag::Number(num) => {
+                let payload = self.payloads.get(&num);
+                payload.ok_or(BlockNotFoundError::new(BlockTag::Number(num)))
+            }
+        }
+    }
+
+    fn check_head_age(&self) -> Result<(), NodeError> {
+        let synced_slot = self.consensus.get_header().slot;
+        let expected_slot = self.consensus.expected_current_slot();
+        let slot_delay = expected_slot - synced_slot;
+
+        if slot_delay > 10 {
+            return Err(NodeError::OutOfSync(slot_delay));
+        }
+
+        Ok(())
+    }
+
+    fn check_blocktag_age(&self, block: &BlockTag) -> Result<(), NodeError> {
+        match block {
+            BlockTag::Latest => self.check_head_age(),
+            BlockTag::Finalized => Ok(()),
+            BlockTag::Number(_) => Ok(()),
+        }
+    }
+}
